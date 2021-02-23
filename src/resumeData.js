614279d9eefc094728dc34294c06b0dd526c87fc
Travis Ko <tkospeaks@gmail.com>
2021-02-22 18:47:25 -0800

    Switched templates, file change

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Travis Ko",
+    "role": "Full Stack Programmer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I am an aspiring full stack developer, with a background in React, Javascript, jQuery, Express and Node. I am always motivated to solve complex issues and take intrigue in a challenge! My background is primarily in finance, with basic coding experience and I am currently enrolled in the UCSD Coding Bootcamp.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/travisko/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/techkospeaks",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "email":"tkospeaks@gmail.com",
+    "website":"https://techkospeaks.github.io/polished-portfolio",
+    "education":[
+      {
+        "UniversityName":"Soka University of America",
+        "specialization":"Social and Behavioral Sciences Degree",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2012",
+        "Achievements":"Captain of the Soccer team two years, 3.6 cumulative GPA, full athletic and merit based scholarship"
+      },
+      {
+        "UniversityName":"Garfield High School",
+        "specialization":"Art based",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2008",
+        "Achievements":"3.8 GPA, Graduated Cum Laude, varsity soccer team 3 years, captain senior year"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Symitar/ Mindseeker Inc.",
+        "specialization":"Application Support Analyst",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2020",
+        "Achievements":"Worked for a year and a half in basic programming and analyst position for a financial company Symitar, working closely with clients to migrate financial service databases and systems"
+      },
+      {
+        "CompanyName":"First Tech Federal Credit Union",
+        "specialization":"Financial Service Advisor",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019",
+        "Achievements":"Served as a financial advisor at First Tech Federal Credit Union, peforming financial related duties for customers and members."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This guy is a wonderful individual, so talented at coding with an outlandishly birght future.",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"He knows things.",
+        "name":"Coding Friends"
+      }
+    ]
+  }
+  
+  export default resumeData
