f9730329f2d6812f546f3b631eb95f0ed06a1fed
Travis Ko <tkospeaks@gmail.com>
2021-02-23 00:15:12 -0800

    Added buttons and final styling elements

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -13,7 +13,8 @@ export default class ContactUs extends Component {
         <div className="row">
           <aside className="eigth columns footer-widgets">
             <div className="widget">
-            <h4> LinkedIn : <a href="https://www.linkedin.com/in/travisko/" target="_blank">LinkedIn Profile</a></h4>
+              <h4> MY RESUME : <a href="https://docs.google.com/document/d/1iMJFDzid7c-mCA4b2vQoJPSLNOII7lex9CM5JbsaqaU/edit?usp=sharing" target="_blank">Resume Link</a></h4>
+              <h4> LinkedIn : <a href="https://www.linkedin.com/in/travisko/" target="_blank">LinkedIn Profile</a></h4>
               <h4> Email : <a
                 href="https://mail.google.com/mail/u/0/#inbox?compose=new">tkospeaks@gmail.com</a></h4>
               <h4>Github : <a
